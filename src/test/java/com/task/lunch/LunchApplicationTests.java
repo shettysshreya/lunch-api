8b8c5502b983c8f7f2dc054bdd335810f8d25627
shettysshreya <sudhir@Sudhirs-MacBook-Pro.local>
2020-10-14 10:29:28 +1100

    Initial commit to set up Spring Boot project structure

---
+++ src/test/java/com/task/lunch/LunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.lunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
